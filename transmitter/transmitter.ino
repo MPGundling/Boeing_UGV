6e958de8a521033a1c6b629eb970d1af8090d335
Mark Gundling <mark.gundling@chartlytics.com>
2018-11-20 13:28:29 -0500

    Working Hello World Wireless example

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,30 +1,24 @@
-#include <Wire.h>
+/*
+* Arduino Wireless Communication Tutorial
+*     Example 1 - Transmitter Code
+*                
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
+#include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#include <RF24_config.h>
-#include <printf.h>
-
-RF24 transmit (2,3);        //create RF24 object called transmit
-
-byte address [6] = "0001";  //set address to 00001
-
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
 void setup() {
-  transmit.begin();
-  transmit.openWritingPipe(address);      //open writing pipe to address 0001
-  transmit.setPALevel(RF24_PA_MIN);      //set RF power to minimum
-  transmit.setDataRate(RF24_250KBPS);    //set datarate to 250kbps
-  transmit.setChannel(100);               //set freq to channel 100
-  transmit.stopListening();
-
+  radio.begin();
+  radio.openWritingPipe(address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.stopListening();
 }
-
 void loop() {
-  char data;
-
-  if(Serial.available()>0)
-  {
-    data = Serial.read();
-    transmit.write(&data,sizeof(data));
-  }
-delay(5);
+  const char text[] = "Hello World";
+  radio.write(&text, sizeof(text));
+  delay(1000);
 }
--- vehicle/vehicle.ino
+++ vehicle/vehicle.ino
@@ -1,131 +1,27 @@
-
-#include <Wire.h>
-#include <Adafruit_MotorShield.h>
+/*
+* Arduino Wireless Communication Tutorial
+*       Example 1 - Receiver Code
+*                
+* by Dejan Nedelkovski, www.HowToMechatronics.com
+* 
+* Library: TMRh20/RF24, https://github.com/tmrh20/RF24/
+*/
+#include <SPI.h>
 #include <nRF24L01.h>
 #include <RF24.h>
-#include <RF24_config.h>
-#include <printf.h>
-
-// Create the motor shield object with the default I2C address
-Adafruit_MotorShield AFMS = Adafruit_MotorShield(); 
-// Or, create it with a different I2C address (say for stacking)
-// Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x61); 
-
-// Left motor (M1)
-Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
-//Right Motor (M2)
-Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
-// Intake motor
-Adafruit_DCMotor *intakeMotor = AFMS.getMotor(3);
-
-RF24 receive (2,3);                         //create object called receive
-byte address [6] = "00001";                 //creat an array with 5 elements, where each element is 1 byte;
-
-int incomingbyte = 0;
-int _speed = 64;
+RF24 radio(7, 8); // CE, CSN
+const byte address[6] = "00001";
 void setup() {
-  //setup serial terminal
   Serial.begin(9600);
-  Serial.println("Boeing UGV - Press 'W' for forward, 'S' for reverse, 'A' for left, 'D' for right, 'SPACE' for stop");
- 
-  //setup motor controller 
-  AFMS.begin();  // create with the default frequency 1.6KHz
-
-  //setup receiever
-  receive.begin();
-  receive.openReadingPipe(0,address);      //open reading pipe 0 at address 00001 
-  receive.setPALevel(RF24_PA_MIN);         //Set RF output to minimum
-  receive.setDataRate(RF24_250KBPS);       //set datarate to 250kbps
-  receive.setChannel(100);                 //set frequency to channel 100 
-  receive.startListening();
+  radio.begin();
+  radio.openReadingPipe(0, address);
+  radio.setPALevel(RF24_PA_MIN);
+  radio.startListening();
 }
-
 void loop() {
-  // put your main code here, to run repeatedly:
- if(receive.available())
- {
-  char incomingbtye;
-  receive.read(&incomingbyte,sizeof(incomingbtye));
- }
- switch(incomingbyte)
- {
-  case '1':
-    Serial.println("25% Power");
-    _speed = 64; 
-    incomingbyte = '*';
-    break;
-  
-  case '2':
-    Serial.println("50% Power");
-    _speed = 128;
-    incomingbyte='*';
-    break;
-  
-  case '3':
-    Serial.println("100% Power");
-    _speed = 250;
-    incomingbyte='*';
-    break;
-  
-  case 'w':
-    Serial.println("Forward");
-    for (int i=0; i<(_speed); i++){
-      leftMotor->setSpeed(i);
-      rightMotor->setSpeed(i);
-      leftMotor->run(FORWARD);
-      rightMotor->run(FORWARD);
-      delay(4);
-    }
-    
-    incomingbyte = '*';
-    break;
-  
-  case 's':
-    Serial.println("Backward");
-    leftMotor->setSpeed(150);
-    rightMotor->setSpeed(150);
-    leftMotor->run(BACKWARD);
-    rightMotor->run(BACKWARD);
-    incomingbyte = '*';
-    break;
-    
-  case 'a':
-    Serial.println("Left");
-    leftMotor->setSpeed(0);
-    rightMotor->setSpeed(150);
-    leftMotor->run(RELEASE);
-    rightMotor->run(FORWARD);
-    incomingbyte = '*';
-    break;
- 
-  case 'd':
-    Serial.println("Right");
-    leftMotor->setSpeed(150);
-    rightMotor->setSpeed(0);
-    leftMotor->run(FORWARD);
-    rightMotor->run(RELEASE);
-    incomingbyte = '*';
-    break;
-  case 't':
-    Serial.println("Current test. All on");
-    intakeMotor->setSpeed(_speed);
-    leftMotor->setSpeed(_speed);
-    rightMotor->setSpeed(_speed);
-    intakeMotor->run(FORWARD);
-    leftMotor->run(FORWARD);
-    rightMotor->run(FORWARD);
-    incomingbyte='*';
-    break;
-   case ' ':
-    Serial.println("STOP");
-    leftMotor->setSpeed(0);
-    rightMotor->setSpeed(0);
-    leftMotor->run(RELEASE);
-    rightMotor->run(RELEASE);
-    intakeMotor->run(RELEASE);
-    incomingbyte = '*';
-    break;
-  
-  delay(10);
- }//end switch
-}//end void loop
+  if (radio.available()>0) {
+    char text[32] = "";
+    radio.read(&text, sizeof(text));
+    Serial.println(text);
+  }
+}
