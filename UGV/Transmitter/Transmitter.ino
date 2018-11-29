167ff0634ff64d9596946dcd9e64266297728d5b
Mark Gundling <mark.gundling@chartlytics.com>
2018-11-29 08:51:02 -0500

    Setup of joystick constants. Changed baud rate to 115200 for testing

--- UGV/Transmitter/Transmitter.ino
+++ UGV/Transmitter/Transmitter.ino
@@ -2,28 +2,33 @@
 #include <printf.h>
 #include <RF24.h>
 #include <RF24_config.h>
+
 //nRF24 constants and variables
 String serialdata; //Holds data coming in
 char msg[32]; //Char array to send data out with
 RF24 transmit (7,8);                    //create RF24 object called transmit
 byte address [6] = "00001";             //set address to 00001
 
-//Joystick pins
+//Joystick constants
 const int left_y_axis = 0;             //left joystick y-axis for left motor control (pin A0)
 const int right_y_axis = 1;            //right joystick y-axis for right motor control (pin A1)
 const int pan_x_axis = 2;              //pan/tilt joystick x axis for pan control (Pin A2)
 const int tilt_y_axis = 3;             //pan/tilt joystick y-axis for tilt control (pin A3)
 
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   Serial.println ("Serial Port Open");
-  
+
+  //wireless setup
   transmit.begin();
   transmit.openWritingPipe(address);    //open writing pipe to address 00001
   transmit.setPALevel(RF24_PA_MAX);     //set RF power output to maximum
   transmit.setDataRate(RF24_250KBPS);   //set datarate to 250kbps
-  transmit.setChannel(20);             //set frequency to channel 20
-  transmit.stopListening();   
+  transmit.setChannel(20);              //set frequency to channel 20
+  transmit.stopListening();             //stop receiving
+
+  //button digital pin setup (for whatever buttons we need)
+   
    }
 
 void loop() {
