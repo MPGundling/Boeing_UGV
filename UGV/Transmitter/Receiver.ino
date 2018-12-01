c4fc3be9b6a4aa0a886801b4144e113c8a38c979
climc118 <cxl5524@psu.edu>
2018-12-01 14:11:56 -0500

    setup for receiver

--- UGV/Transmitter/Receiver.ino
+++ UGV/Transmitter/Receiver.ino
@@ -3,13 +3,21 @@
 #include <RF24.h>
 #include <RF24_config.h>
 
-RF24 transmit (7,8);   
+RF24 receiver(7,8);   
 byte address [6] = "00001";   
 
 void setup() {
-
+    Serial.begin(9600);
+    receiver.begin();
+    receiver.openReadingPipe(0, address);
+    receiver.setPALevel(RF24_PA_MIN);
+    receiver.startListening();
 }
 
 void loop() {
-    
+    if (radio.available()) {
+        char incoming[6] = "";
+        receiver.read(&incoming, sizeof(incoming));
+        Serial.println(incoming);
+    }
 }
