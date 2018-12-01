41e1575f236ddd65845a1a285c3053d84ffab1e8
climc118 <cxl5524@psu.edu>
2018-12-01 14:04:45 -0500

    nrf vars, arduino functions

---
+++ UGV/Transmitter/Receiver.ino
@@ -0,0 +1,15 @@
+#include <nRF24L01.h>
+#include <printf.h>
+#include <RF24.h>
+#include <RF24_config.h>
+
+RF24 transmit (7,8);   
+byte address [6] = "00001";   
+
+void setup() {
+
+}
+
+void loop() {
+    
+}
