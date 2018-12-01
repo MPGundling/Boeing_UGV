fe9132fa105c29c5324b27c57ddab75a9757b1ff
climc118 <cxl5524@psu.edu>
2018-12-01 15:01:02 -0500

    motor re-assignment

--- UGV/Receiver/Receiver.ino
+++ UGV/Receiver/Receiver.ino
@@ -12,11 +12,11 @@ byte address [6] = "00001";
 // Adafruit Motorshield variable instantiation.
 Adafruit_MotorShield AFMS = Adafruit_MotorShield();
 // Left motor (M1)
-Adafruit_DCMotor *leftMotor = AFMS.getMotor(1);
+Adafruit_DCMotor *leftMotor = AFMS.getMotor(3);
 //Right Motor (M2)
-Adafruit_DCMotor *rightMotor = AFMS.getMotor(2);
+Adafruit_DCMotor *rightMotor = AFMS.getMotor(4);
 // Intake motor
-Adafruit_DCMotor *intakeMotor = AFMS.getMotor(3);
+Adafruit_DCMotor *intakeMotor = AFMS.getMotor(2);
 
 void setup() {
     Serial.begin(9600);
@@ -34,6 +34,7 @@ void loop() {
         receiver.read(&incoming, sizeof(incoming));
         Serial.println(incoming);
     }
+
     if (incoming[4] == 0) {
         intakeMotor -> setSpeed(255);
         intakeMotor -> run(FORWARD);
